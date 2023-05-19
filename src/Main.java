6c530b122444b4475c84b60e553214a6b864c82f
Neto test <sysl86@mail.ru>
2023-05-19 20:14:13 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 8750;
+        int mille = 20;
+        int bonus = amount / mille;
+
+        System.out.println(bonus);
+    }
+}
